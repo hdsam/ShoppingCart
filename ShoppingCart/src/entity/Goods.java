6d76de9bd495049574c417f7ca50df16c081e2ba
hdsam <371182707@qq.com>
2016-11-20 18:46:35 +0800

    done!not very well!

--- ShoppingCart/src/entity/Goods.java
+++ ShoppingCart/src/entity/Goods.java
@@ -1,29 +1,70 @@
 package entity;
 
-public class Goods {
+import java.io.Serializable;
+
+public class Goods implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private int goodsId;
 	private String goodsName;
-	private String goodsPrice;
+	private int goodsPrice;
+	private int goodsNum;
+
+	public int getGoodsNum() {
+		return goodsNum;
+	}
+
+	public void setGoodsNum(int goodsNum) {
+		this.goodsNum = goodsNum;
+	}
+
 	public int getGoodsId() {
 		return goodsId;
 	}
+
 	public void setGoodsId(int goodsId) {
 		this.goodsId = goodsId;
 	}
+
 	public String getGoodsName() {
 		return goodsName;
 	}
+
 	public void setGoodsName(String goodsName) {
 		this.goodsName = goodsName;
 	}
-	public String getGoodsPrice() {
+
+	public int getGoodsPrice() {
 		return goodsPrice;
 	}
-	public void setGoodsPrice(String goodsPrice) {
-		this.goodsPrice = goodsPrice;
+
+	public void setGoodsPrice(int goodsprice) {
+		this.goodsPrice = goodsprice;
 	}
+
 	public String toString() {
 		return "Goods [goodsId=" + goodsId + ", goodsName=" + goodsName + ", goodsPrice=" + goodsPrice + "]";
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return this.getGoodsId() + this.getGoodsName().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj instanceof Goods) {
+			Goods g = (Goods) obj;
+			return this.goodsId == g.goodsId && this.goodsName.equals(g.goodsName);
+
+		} else {
+			return false;
+		}
+
+	}
+
 }
--- ShoppingCart/src/test/Test.java
+++ ShoppingCart/src/test/Test.java
@@ -1,15 +1,53 @@
 package test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import dao.GoodsDaoImpl;
+import dbUtil.DBConnectionFactory;
+import entity.Goods;
+
 public class Test {
-	public static int n=1;
-	static{
-			n=10;
-	}
-	
-	public static int get(){ return n;};
+
 	public static void main(String[] args) {
-		
-		System.out.println(Test.get());
+//		ArrayList<Goods> goodsList =new ArrayList<Goods>();
+//		DBConnectionFactory dcf=DBConnectionFactory.getInstance();
+//		Connection	conn=dcf.getConnection();
+//		String sql="select * from tbl_goods";
+//		ResultSet rs=null;
+//		try {
+//			PreparedStatement pst=conn.prepareStatement(sql);
+//			rs=pst.executeQuery();
+//			while (rs.next()) {
+//				Goods g=new Goods();
+//				g.setGoodsId(rs.getInt("goodsId"));
+//				g.setGoodsName(rs.getString("goodsName"));
+//				g.setGoodsPrice(rs.getString("goodsPrice"));
+//				g.setGoodsNum(rs.getInt("goodsNum"));
+//				goodsList.add(g);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	
+//		for(int i=0;i<goodsList.size();i++){
+//			Goods goods=goodsList.get(i);
+//			System.out.print(goods.getGoodsId());
+//			System.out.print(goods.getGoodsName());
+//			System.out.print(goods.getGoodsPrice());
+//			System.out.println(goods.getGoodsNum());
+//			}
+		GoodsDaoImpl dgi=new GoodsDaoImpl();
+		HashMap<Goods, Integer>goodsMap=dgi.queryGoodsFromCart();
+		Iterator<Goods> gs=goodsMap.keySet().iterator();
+		while(gs.hasNext()){
+			gs.next().toString();
+		}
 	}
 
 }
